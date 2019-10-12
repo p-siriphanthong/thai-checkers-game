2cd944dd2053a4df44065d56dc521c97241ce811
Punchaya Siriphanthong <p.siriphanthong@gmail.com>
2019-10-12 23:53:43 +0700

    Updates

---
+++ precache-manifest.7f9aed89d6eac251361e44674180134d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a51cff58eaaf3c234a88f3968b1b526",
+    "url": "/thai-checkers-game/index.html"
+  },
+  {
+    "revision": "8185a72e77d5de56e0a5",
+    "url": "/thai-checkers-game/static/js/2.0f11017c.chunk.js"
+  },
+  {
+    "revision": "be9201e3d9821e323dde",
+    "url": "/thai-checkers-game/static/js/main.af8aae25.chunk.js"
+  },
+  {
+    "revision": "61dd766344be10b1222a",
+    "url": "/thai-checkers-game/static/js/runtime-main.13bab532.js"
+  }
+]);
